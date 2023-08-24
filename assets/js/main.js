d08dcdedc0666a22455df3e800d128131f6241e3
Ayush Jain <Ayush.Jain@vanarts.com>
2023-08-23 19:44:18 -0700

    main css and js changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,17 @@ const app = new Application(canvas);
 app.load('https://prod.spline.design/QOjHjWLstckyHVQS/scene.splinecode');
 
 
+const navbar = document.querySelector('.nav');
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
+
 
 (function($) {
 
